2402f121219b36badaa203388bfc3e03f91576e9
profeweb <42413333+mitjanit@users.noreply.github.com>
2019-09-20 11:13:25 +0200

    Update script08.py

--- venv/script08.py
+++ venv/script08.py
@@ -3,18 +3,15 @@ import random
 import math
 
 z = -100
-radius = 0
-theta = 0
+radius = 30
+pi = math.pi
 
 while z <=100 :
-    x = radius*math.cos(theta)
-    y = radius*math.sin(theta)
-    point = (x,y,z)
-    rs.AddPoint(point)
-    if z>20 and z<80 :
-        radius = radius - .003
-    else :
-        radius = radius + .003
-
-    theta += 1
-    z += .01
+    theta = 0
+    while theta < 2*pi :
+        x = radius*math.cos(theta)
+        y = radius*math.sin(theta)
+        point = (x,y,z)
+        rs.AddPoint(point)
+        theta += .1
+    z += .5
