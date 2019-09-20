07c18f7f16ee3bbe5df5d97e4001b9ebc9edaaa9
profeweb <42413333+mitjanit@users.noreply.github.com>
2019-09-20 11:16:09 +0200

    Script 09

---
+++ venv/script09.py
@@ -0,0 +1,18 @@
+import rhinoscriptsyntax as rs
+import random
+import math
+
+z = -100
+radius = 40
+pi = math.pi
+
+while z <=100 :
+    theta = 0
+    while theta < 2*pi :
+        randomShift = random.uniform(-1,1)*(z/25.0)
+        x = radius*math.cos(theta) + randomShift
+        y = radius*math.sin(theta) + randomShift
+        point = (x,y,z)
+        rs.AddPoint(point)
+        theta += .5 #random.uniform(-1.0,1.0)*z
+    z += .3
